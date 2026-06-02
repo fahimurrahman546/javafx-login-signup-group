325476f213a29dd9655b82dd5bf6396af3d09564
Fahim <fahimurrahman546@gmail.com>
2026-06-02 22:36:57 +0600

    Updated login page UI

--- src/com/app/Login.java
+++ src/com/app/Login.java
@@ -8,19 +8,29 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javafx.scene.text.Font;
 
 public class Login extends Application {
 
     @Override
     public void start(Stage stage) {
-        Label title=new Label("Login SYSTEM");
-        TextField username=new TextField();
-        username.setPromptText("Enter Username");
+        Label title=new Label("Login System");
+        title.setFont(new Font("Comic Sans MS", 22));
+        TextField username = new TextField();
+username.setPromptText("Enter Username");
+username.setMaxWidth(250);
         
-        PasswordField password= new PasswordField();
-        password.setPromptText("Enter Password");
+       PasswordField password = new PasswordField();
+password.setPromptText("Enter Password");
+password.setMaxWidth(250);
 Button loginBtn = new Button("Login");
+loginBtn.setStyle(
+    "-fx-background-color: #007BFF;" +
+    "-fx-text-fill: white;" +
+    "-fx-background-radius: 8px;"
+);
 VBox root = new VBox(15);
 root.getChildren().addAll(
     title,username,password,loginBtn
